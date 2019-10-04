1d95328f659ba0c4542b58ffe3d971a5bfed9a0b
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-04 18:43:41 +0700

    frist commit

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+
+// 1 Khai báo biến
+
+// 2 Toán tử
+
+//3 Câu điều kiện
+
+//4 Vòng lặp
+//5 Object method
+//6 Array method
+//7 OOP
+//8 Xu ly file
+ $a = "Hello";
+ $a = "Xin chào";
+ echo $a;
+
+?>
