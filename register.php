31b15aadfc56685e3554d33da87b803a846ec2fd
minhnc02082000 <53040823+minhnc02082000@users.noreply.github.com>
2019-10-07 19:12:55 +0700

    A

---
+++ register.php
@@ -0,0 +1,22 @@
+<?php
+    require("connect.php");
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $insertAccount = "INSERT INTO user VALUES (null , '$username' , '$password')";
+    $data = mysqli_query($con,$insertAccount);
+// 1 : UserResponse -> class cho viec tra ve
+    class UserResponse{
+        function __construct($success , $message){
+            $this->success=$success;
+            $this->message=$message;
+        }
+    }
+    // { "success " : true , "message" : "Thanh cong"}
+    // { "success " : false , "message" : "That bai"}
+    if(mysqli_error($con)){
+        echo json_encode(new UserResponse(false,"That bai"));
+    }else{
+        echo  json_encode(new UserResponse(true,"Thanh cong"));
+    }
+   
+?>
